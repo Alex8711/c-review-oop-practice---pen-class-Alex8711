d0e036f146924e6485cf493ce24c93f73da8e513
Alex8711 <liuzhendell@gmail.com>
2020-10-21 11:15:53 -0600

    Add project files.

---
+++ c-review-oop-practice---pen-class-Alex8711/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_review_oop_practice___pen_class_Alex8711
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
